48d19bd45b49895c7abc5c68da60900784ea43fb
Rhoenicx <rhoenicx@gmail.com>
2022-01-20 20:11:49 +0100

    Updated to proper mod

--- HairLoader.cs
+++ HairLoader.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using Terraria;
 using Terraria.ModLoader;
 using Terraria.ModLoader.IO;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace HairLoader
 {
@@ -11,37 +12,86 @@ namespace HairLoader
     {
         public Texture2D Hair { get; set; }
         public Texture2D HairAlt { get; set; }
-	public string Name { get; set; }
     }
     
     class HairLoader : Mod
     {
-        public static Dictionary<int, PlayerHair> HairStyles = new Dictionary<int, PlayerHair>();
-    
-        public override void load()
-        {
-	    if (!Main.dedServ)
-            {
+		// Stores all the hairstyles based on ID
+		public static Dictionary<int, PlayerHair> HairStyles = new Dictionary<int, PlayerHair>();
 
-            }
-	}
+		// stores which mod added which texture
+		public static Dictionary<string, Dictionary<int, int>> HairTable = new Dictionary<string, Dictionary<int, int>>();
 		
-	public int AddCustomHairStyle(Texture2D hair, Texture2D hairAlt, string name)
-	{
-	    int index = HairStyles.Keys.Max() >= Main.maxHairLoaded ? HairStyles.Keys.Max() + 1 : Main.maxHairLoaded;
-			
-	    HairLoader.HairStyles.Add
-            (
-	        index,
-		new HairLoader.PlayerHair
-		{ 
-		    Hair = hair,
-		    HairAlt = hairAlt,
-		    Name = name
-	        }
-	    );
-			
-	    return index;
+		// stores which hairStyle each player is wearing
+		public static int[] PlayerDisplayHairStyle = new int[128];
+
+        public override void Load()
+        {
+	    	if (!Main.dedServ)
+        	{
+
+        	}
+		}
+
+		public override void Unload()
+		{
+			if (!Main.dedServ)
+        	{
+				HairStyles = null;
+				HairTable = null;
+				PlayerDisplayHairStyle = null;
+        	}
+		}
+		
+//-----------------------------------------------------------------------------------------------------------------------------
+		public void RegisterCustomHair(string modName, Texture2D hair, Texture2D hairAlt)
+		{
+			int index = HairStyles.Keys.Max() >= Main.maxHairTotal ? HairStyles.Keys.Max() + 1 : Main.maxHairTotal;
+
+			HairLoader.HairStyles.Add
+			(
+				index,
+				new PlayerHair
+				{
+					Hair = hair,
+					HairAlt = hairAlt,
+				}
+			);
+
+			if (!HairTable.ContainsKey(modName))
+			{
+				HairTable.Add(modName, new Dictionary<int, int>());
+			}
+
+			int subindex = 0;
+			if (HairTable[modName].Count != 0)
+			{
+				subindex = HairTable[modName].Keys.Max() + 1;
+			}
+
+			HairTable[modName].Add(subindex, index);
+		}
+
+		public bool ChangePlayerHairStyle(string modName, int modHairID, int PlayerID)
+		{
+			if (!HairTable.ContainsKey(modName))
+			{
+				return false;
+			}
+
+			if (!HairTable[modName].ContainsKey(modHairID))
+			{
+				return false;
+			}
+
+			PlayerDisplayHairStyle[PlayerID] = HairTable[modName][modHairID];
+
+			return true;
+		}
+
+		public void ClearPlayerDisplayHairStyle()
+		{
+			PlayerDisplayHairStyle = new int[128];
+		}
 	}
-    }
 }	
--- HairLoaderPlayer.cs
+++ HairLoaderPlayer.cs
@@ -6,39 +6,38 @@ using Terraria.ModLoader;
 
 namespace HairLoader
 {
-    public class HairLoaderPlayer : modPlayer
+    public class HairLoaderPlayer : ModPlayer
     {
         public int HairStyleID = -1;
 
-        public override void ModifyDrawInfo(PlayerDrawInfo drawInfo)
-        {	
-            // ALWAYS check if we have the vanilla hairstyle loaded
-            if (!HairLoader.HairStyles.ContainsKey(drawInfo.drawPlayer.hair))
-            {
-                HairLoader.HairStyles.Add
-                (
-                    drawInfo.drawPlayer.hair,
-                    new HairLoader.PlayerHair
-                    { 
-                        Hair = Main.playerHairTexture[drawInfo.drawPlayer.hair],
-                        HairAlt = Main.playerHairAltTexture[drawInfo.drawPlayer.hair],
-                        Name = "Vanilla hairstyle" + drawInfo.drawPlayer.hair.ToString()
-                    }
-                );
-            }
-		
-            // Now we check if the custom hairstyle has been loaded
-            int HairStyleID = drawInfo.drawPlayer.GetModPlayer<HairLoaderPlayer>().HairStyleID;
-			
-            if (!HairLoader.HairStyles.ContainsKey(HairStyleID))
-            {
-                // if not loaded reset the player's hairstyle
-                drawInfo.drawPlayer.GetModPlayer<HairLoaderPlayer>().HairStyleID = drawInfo.drawPlayer.hair;
-                HairStyleID = drawInfo.drawPlayer.hair;	
-            }
-			
-            Main.playerHairTexture[drawInfo.drawPlayer.hair] = HairLoader.HairStyles[HairStyleID].Hair;
-	    Main.playerHairAltTexture[drawInfo.drawPlayer.hair] = HairLoader.HairStyles[HairStyleID].Hair;
-        }
+        public override void ModifyDrawInfo(ref PlayerDrawInfo drawInfo)
+		{
+			// ALWAYS check if we have the vanilla hairstyle loaded
+			if (!HairLoader.HairStyles.ContainsKey(drawInfo.drawPlayer.hair))
+			{
+				HairLoader.HairStyles.Add
+				(
+					drawInfo.drawPlayer.hair,
+					new PlayerHair
+					{
+						Hair = Main.playerHairTexture[drawInfo.drawPlayer.hair],
+						HairAlt = Main.playerHairAltTexture[drawInfo.drawPlayer.hair]
+					}
+				);
+			}
+
+			// Now we check if the custom hairstyle has been loaded
+			int HairStyleID = HairLoader.PlayerDisplayHairStyle[drawInfo.drawPlayer.whoAmI];
+
+			if (!HairLoader.HairStyles.ContainsKey(HairStyleID))
+			{
+				// if not loaded reset the player's hairstyle to the default one
+				HairLoader.PlayerDisplayHairStyle[drawInfo.drawPlayer.whoAmI] = drawInfo.drawPlayer.hair;
+				HairStyleID = drawInfo.drawPlayer.hair;
+			}
+
+			Main.playerHairTexture[drawInfo.drawPlayer.hair] = HairLoader.HairStyles[HairStyleID].Hair;
+			Main.playerHairAltTexture[drawInfo.drawPlayer.hair] = HairLoader.HairStyles[HairStyleID].Hair;
+		}
     }
 }
